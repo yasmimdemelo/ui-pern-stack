7efaa6bad9842cbe1030039a00e9f9eeb4954b19
Yasmim <yasmimdmf95@gmail.com>
2023-02-09 11:45:13 -0300

    fix: change index.js to render app.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+//root-dom faz com que possamos trabalhar com o cor do react na dom(web, mobile)
+////createRoot é uma função js que vai receber quem é o elemento raiz "rootelement".
+////rootelement armazena o valor encontrado na root, para usar no createRoot.
+import { createRoot} from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
+//Aqui vamos importar o App.js, que contem a função App( ) com os componentes.
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+//root que irá renderizar o conteudo de App( ), 
+root.render(
+  <App />
+);
